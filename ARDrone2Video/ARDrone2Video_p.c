b45eeb24dcb5ddc32e0998f839dff2070422b670
danvy <alex@danvy.tv>
2013-08-03 18:41:31 +0200

    updates

--- ARDrone2Video/ARDrone2Video_p.c
+++ ARDrone2Video/ARDrone2Video_p.c
@@ -4,9 +4,9 @@
 
 
  /* File created by MIDL compiler version 8.00.0595 */
-/* at Thu Aug 01 18:33:25 2013
+/* at Sat Aug 03 03:05:34 2013
  */
-/* Compiler settings for C:\Users\aldanvy\AppData\Local\Temp\ARDrone2Video.idl-594c623f:
+/* Compiler settings for C:\Users\aldanvy\AppData\Local\Temp\ARDrone2Video.idl-8c65c850:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 8.00.0595 
     protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
